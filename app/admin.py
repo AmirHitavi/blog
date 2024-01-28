9a5c2757718bf7806b8250abb980a79d707c430b
AmirHitavi <a.hitavi@yahoo.com>
2024-01-28 09:51:21 +0330

    models for blog added

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Profile, Tag, Post, Comment, Subscribe, WebsiteMeta
 # Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Subscribe)
+admin.site.register(WebsiteMeta)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_image = models.ImageField(upload_to="images/", null=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.user.username)
+        return super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, null=True, related_name="post")
+    view_count = models.IntegerField()
+    is_featured = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    bookmarks = models.ManyToManyField(User, related_name="bookmarks", default=None, blank=True)
+    likes = models.ManyToManyField(User, related_name="likes", default=None, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    email = models.EmailField(max_length=255)
+    website = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True, related_name="replies")
+
+    def __str__(self):
+        if self.parent:
+            return f"reply for {self.post.title}"
+        else:
+            return f"comment for {self.post.title}"
+
+
+class Subscribe(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
+class WebsiteMeta(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    about = models.TextField()
+
+    def __str__(self):
+        return self.title
